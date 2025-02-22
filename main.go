0185167fdde95eb893d89b62ad0d13553510110c
Ruben Voskanyan <artemy.chexov@mail.ru>
2025-02-22 15:32:52 +0400

    A task related to the months and days in which the Switch statement was used.

--- main.go
+++ main.go
@@ -2,21 +2,37 @@ package main
 
 import "fmt"
 
-func checkAge(age int) string{
-	if age < 18{
-		return "Вы несовершенолетний"
-	} else if age <= 60{
-		return "Вы совершенолетний"
-	} else {
-		return "Вы пенсионер"
+func main() {
+
+	var num int
+
+	fmt.Print("Введите число месяца (0-12): ")
+	fmt.Scan(&num)
+
+	switch num {
+	case 1:
+		fmt.Println("В этом месяце: 31 дней. ")
+	case 2:
+		fmt.Println("В этом месяце: 28 дней. ")
+	case 3:
+		fmt.Println("В этом месяце: 31 дней. ")
+	case 4:
+		fmt.Println("В этом месяце: 30 дней. ")
+	case 5:
+		fmt.Println("В этом месяце: 31 дней. ")
+	case 6:
+		fmt.Println("В этом месяце: 30 дней. ")
+	case 7,8:
+		fmt.Println("В этом месяце: 31 дней. ")
+	case 9:
+		fmt.Println("В этом месяце: 30 дней. ")
+	case 10:
+		fmt.Println("В этом месяце: 31 дней. ")
+	case 11:
+		fmt.Println("В этом месяце: 30 дней. ")
+	case 12:
+		fmt.Println("В этом месяце: 31 дней. ")
+	default:
+		fmt.Println("Ошибка: неизвестная операция")
 	}
-}
-
-func main(){
-	var age int
-	fmt.Println("Введите ваш возраст: ")
-	fmt.Scan(&age)
-
-	result := checkAge(age)
-	fmt.Println(result)
 }
