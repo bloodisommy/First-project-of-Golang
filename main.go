c351104a3b445fc522e44e7424872e79832da65b
Ruben Voskanyan <artemy.chexov@mail.ru>
2025-02-25 14:30:22 +0400

    Using methods in code that performs a small task

--- main.go
+++ main.go
@@ -2,20 +2,39 @@ package main
 
 import "fmt"
 
-type Person struct{
-    Name string
-    Age int
+type BankAccount struct {
+    Balance       float64
+    AccountHolder string
 }
 
-func incrementAge(p *Person){
-    p.Age = p.Age + 1
+func (b *BankAccount) Deposit(amount float64) {
+    b.Balance += amount
+}
+
+func (b *BankAccount) Withdraw(amount float64) {
+    if b.Balance >= amount {
+        b.Balance -= amount
+    } else {
+        fmt.Println("Ошибка: недостаточно средств.")
+    }
+}
+
+func (b BankAccount) PrintAccountInfo() {
+    fmt.Printf("Владелец счета: %s\nБаланс: %.2f\n", b.AccountHolder, b.Balance)
 }
 
 func main() {
-  person := Person{Name: "Ruben, ", Age: 17}
-  fmt.Println("До изменений: ", person)
+    
+    account := BankAccount{AccountHolder: "Ruben Voskanyan", Balance: 1000.50}
 
-  incrementAge(&person)
+    account.PrintAccountInfo()
 
-  fmt.Println("После изменений: ", person)
+    account.Deposit(500)
+    account.PrintAccountInfo()
+
+    account.Withdraw(200)
+    account.PrintAccountInfo()
+
+    account.Withdraw(2000)
+    account.PrintAccountInfo()
 }
