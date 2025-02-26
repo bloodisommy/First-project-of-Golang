a5df3008891f2e97fd8884ac073dd25e7d217849
Ruben Voskanyan <artemy.chexov@mail.ru>
2025-02-26 21:23:14 +0400

    Using the iota pattern and writing an example

--- main.go
+++ main.go
@@ -2,39 +2,14 @@ package main
 
 import "fmt"
 
-type BankAccount struct {
-    Balance       float64
-    AccountHolder string
-}
-
-func (b *BankAccount) Deposit(amount float64) {
-    b.Balance += amount
-}
-
-func (b *BankAccount) Withdraw(amount float64) {
-    if b.Balance >= amount {
-        b.Balance -= amount
-    } else {
-        fmt.Println("Ошибка: недостаточно средств.")
-    }
-}
-
-func (b BankAccount) PrintAccountInfo() {
-    fmt.Printf("Владелец счета: %s\nБаланс: %.2f\n", b.AccountHolder, b.Balance)
-}
-
 func main() {
-    
-    account := BankAccount{AccountHolder: "Ruben Voskanyan", Balance: 1000.50}
 
-    account.PrintAccountInfo()
+    const (
+        Cold = iota * 10-5
+        Warm
+        Hot
+    )
 
-    account.Deposit(500)
-    account.PrintAccountInfo()
+    fmt.Println(Cold, Warm, Hot)
 
-    account.Withdraw(200)
-    account.PrintAccountInfo()
-
-    account.Withdraw(2000)
-    account.PrintAccountInfo()
 }
