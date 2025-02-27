1e12c6db495a5648c375cf885d0be5532e4a675d
Ruben Voskanyan <artemy.chexov@mail.ru>
2025-02-27 21:08:46 +0400

    Using Interfaces and Writing an Example

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-func Age(age int)int{
-    return age
+import "fmt"
+
+type Speaker interface {
+    Speak() int
 }
 
+type Person struct {
+    Age int
+}
+
+func (p Person) Speak() int {
+    return (p.Age)
+}
+
+func main() {
+    var speaker Speaker 
+    speaker = Person{Age: 17} 
+
+    fmt.Println(speaker.Speak())
+}
